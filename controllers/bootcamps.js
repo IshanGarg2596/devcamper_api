326911ed7ae7859b57cb74ff745c674f919e6631
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2022-02-14 22:29:57 +0530

    add controller methods and middleware

---
+++ controllers/bootcamps.js
@@ -0,0 +1,38 @@
+// @desc     Get all bootcamps
+// @route    GET /api/v1/bootcamps
+// @access   public
+exports.getBootcamps = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+};
+
+// @desc     Get single bootcamp
+// @route    GET /api/v1/bootcamps/:id
+// @access   public
+exports.getBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: `get bootcamp ${req.params.id}` });
+};
+
+// @desc     Create new bootcamp
+// @route    POST /api/v1/bootcamps/
+// @access   private
+exports.createBootcamp = (req, res, next) => {
+  res.status(200).json({ success: true, msg: "Create new bootcamp" });
+};
+
+// @desc     Update single bootcamp
+// @route    PUT /api/v1/bootcamps/:id
+// @access   private
+exports.updateBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `Update bootcamp ${req.params.id}` });
+};
+
+// @desc     Delete single bootcamp
+// @route    DELETE /api/v1/bootcamps/:id
+// @access   private
+exports.deleteBootcamp = (req, res, next) => {
+  res
+    .status(200)
+    .json({ success: true, msg: `delete bootcamp ${req.params.id}` });
+};
