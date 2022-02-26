74b31b234aa15144ded0dcf236032d2a219256d0
Ishan  Garg <ishan@Ishans-MacBook-Air.local>
2022-02-27 00:29:55 +0530

    create logout route

--- server.js
+++ server.js
@@ -1,25 +1,63 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-// Route files
-const bootcamps = require("./routes/bootcamps");
+const fileUpload = require("express-fileupload");
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
 
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+const courses = require("./routes/courses");
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const reviews = require("./routes/reviews");
+
+// Connect to database
+connectDB();
+
 const app = express();
 
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+// Dev logging middleware
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+// File uploading
+app.use(fileUpload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
+
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
+app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
+app.use("/api/v1/users", users);
+app.use("/api/v1/reviews", reviews);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
+  // Close server & exit process
+  server.close(() => process.exit(1));
+});
